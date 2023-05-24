d1cccb593d150ad1437df65f47f91a47b21e8f81
G-Lumia <giorgiolumia@gmail.com>
2023-05-24 14:34:17 +0200

    Seeder created and db populated

--- database/seeders/TrainTableSeeder.php
+++ database/seeders/TrainTableSeeder.php
@@ -4,6 +4,8 @@ namespace Database\Seeders;
 
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Faker\Generator as Faker;
+use App\Models\Train;
 
 class TrainTableSeeder extends Seeder
 {
@@ -12,8 +14,22 @@ class TrainTableSeeder extends Seeder
      *
      * @return void
      */
-    public function run()
-    {
-        //
-    }
+
+     public function run(Faker $faker)
+     {
+         for ($i = 0; $i < 10; $i++) {
+             $newTrain = new Train();
+             $newTrain->codice = $faker->lexify();
+             $newTrain->azienda = $faker->company();
+             $newTrain->stazione_partenza = $faker->city();
+             $newTrain->orario_partenza = $faker->time();
+             $newTrain->stazione_arrivo = $faker->city();
+             $newTrain->orario_arrivo = $faker->time();
+             $newTrain->numero_carrozze = $faker->numberBetween(5, 25);
+             $newTrain->in_orario = $faker->boolean();
+             $newTrain->cancellato = $faker->boolean();
+             $newTrain->save();
+         }
+     }
+
 }
